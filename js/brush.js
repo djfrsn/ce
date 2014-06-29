1a6a6d30c91a983a0cb84d463a178250e8e99bb6
Dennis Jefferson <dj@dennisjefferson.com>
2014-06-29 00:02:31 -0700

    Set link animations for 'cd__link & ae__link'

--- js/brush.js
+++ js/brush.js
@@ -1,6 +1,14 @@
-$(document).ready(function() {
-    $('.cd__link').on('hover', function() { 
-        $( '.ce__bg'  ).css('background', 'url(../lib/img/ce__bgLeft.jpg)').fadeIn();
-    });
-});
-
+ $(document).ready(function() {
+        $('.cd__link').mouseover( function() { 
+        $('.ce__bgleft').fadeIn(675);
+        });
+        $('.cd__link').mouseleave( function() { 
+        $('.ce__bgleft').fadeOut(675);
+        });
+        $('.ae__link').mouseover( function() { 
+        $('.ce__bgright').fadeIn(675);
+        });
+        $('.ae__link').mouseleave( function() { 
+        $('.ce__bgright').fadeOut(675);
+        });
+        });
